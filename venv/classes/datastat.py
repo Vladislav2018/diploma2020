7a04f5720eb753a7c054d547f97449eb58d6c177
VLADISLAV <oxxxykpss@gmail.com>
2020-02-24 22:51:53 +0300

    numpy helps with so large distribution
    at least, my pc doesn't freeze

--- venv/classes/datastat.py
+++ venv/classes/datastat.py
@@ -1,5 +1,8 @@
 from random import *
 import math
+from scipy import stats
+import numpy as np
+
 
 def get_sex():
     is_male= [True, False]
@@ -15,6 +18,7 @@ def get_age():
     _w = []
     for age in ages:
         # distribution func
+        #todo: make koef 1.2 dynamic by situation
         _w.append(sum(ages) / math.pow(age, 1.2))
 
     the_age_sample = choices(ages, weights=_w, k=1)
@@ -24,6 +28,12 @@ def get_age():
 def get_job_dist():
     #it was better thanks for @trapwalker from Habr Answers, but not at all
     #todo: solve it normal way, for example with scipy.stats.rv_discrete
-    all_dists = [*range(20,20000000)]
-    dist = choices(all_dists, (1/x for x in all_dists))
+    NEAREST_DIST = 20.0
+    FAR_DIST = 1.8*10**7
+    all_dists = np.arange(NEAREST_DIST, FAR_DIST, 1.0)
+    weights = [1/x for x in all_dists]
+    weights = np.array(weights)
+    weights /= weights.sum()
+    print(weights)
+    dist = np.random.choice(all_dists,1, p = weights)
     return dist
--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,14 @@
 import sys
 from datawriter import *
 from classes.candidate import Candidate
+from datastat import *
 
 def main():
    test= Candidate()
    #print(test.job_dist,  test.male, test.age, '\n', test.bad_habits)
    #print(dir((test)))
-   write(test)
+   #write(test)
+   
 
 
 if __name__== "__main__":
